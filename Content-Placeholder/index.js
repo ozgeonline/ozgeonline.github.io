5dc05ab00ab26f01d44961d9f8e7b74b83eb2ee9
ozgeonline <nisroc.414@hotmail.com>
2023-08-23 15:27:28 +0300

    placeholder

---
+++ Content-Placeholder/index.js
@@ -0,0 +1,22 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const names = document.getElementById('name')
+const date = document.getElementById('date')
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2000)
+
+function getData() {
+  header.innerHTML = '<img src="https://plus.unsplash.com/premium_photo-1676479611993-05b0c9232f84?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1806&q=80" alt="" />'
+  title.innerHTML = 'Lorem ipsum dolor sit amet'
+  excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolore perferendis.Lorem ipsum dolor sit amet consectetur icing elit.Dolore perferendis.'
+  profile_img.innerHTML = '<img src="https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80" alt="" />'
+  names.innerHTML = 'Ozgeonline'
+  date.innerHTML = 'Aug 23, 2023'
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
